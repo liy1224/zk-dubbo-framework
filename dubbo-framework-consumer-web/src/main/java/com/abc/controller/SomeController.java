77957ba90b792510a1207d9871b3d62fab8328f6
Lance1307 <our130727@126.com>
2019-08-20 22:57:47 +0800

    dubbo继承zookeeper

--- dubbo-framework-consumer-web/src/main/java/com/abc/controller/SomeController.java
+++ dubbo-framework-consumer-web/src/main/java/com/abc/controller/SomeController.java
@@ -1,6 +1,6 @@
 package com.abc.controller;
 
-import com.abc.service.SomeService;
+import com.zk.framework.service.api.SomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
